20661e2bd1e7d2587be2ff1ec1b88b21c24e916f
Alex Wan <a_wanderous@hotmail.com>
2019-08-29 16:03:35 +1000

    set up start screen

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+    // set start screen
+    // button to start game
+    $("#start").click(function(){
+        $("#start, header").hide();
+    })
+
+// timer starts when pressed 
+var timer = 30;
+
+// show correct answer 
+
+//  10 questions 
+//  aloha forms (diglet, Shellder, Vulpix, Exeggutor)
+//  leaf type pokemon (Oddish, Bubasaur, Bellsprout)
+//  eevee evolutions
+//  pikachu evolution tree (Pikachu, )
+//  legendary pokemon
+//  Kanto region pokemon
+//  no evolution (Farfetch'd, )
+//  starting pokemon in kanto region
+//  pokedex no.1 pokemon (Bubasaur, Charmander, Squirtle , Pikachu.)
+//  pikachu type
+
+// show score correct out of /10
+
+})
