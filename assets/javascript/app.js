45550cc62080fd984dabfc11d7b776e7f4e31b15
Alex Wan <a_wanderous@hotmail.com>
2019-08-30 15:26:27 +1000

    have questions written with images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,24 +4,29 @@ $(document).ready(function () {
     // button to start game
     $("#start").click(function(){
         $("#start, header").hide();
+        timer = setInterval(function(){
+
+        }, 1000); 
     })
 
-// timer starts when pressed 
-var timer = 30;
-
+    var timer = 30;
+    var correct = 0;
+    var wrong = 0;
+    // timer starts when pressed
+     {
 // show correct answer 
 
 //  10 questions 
-//  aloha forms (diglet, Shellder, Vulpix, Exeggutor)
-//  leaf type pokemon (Oddish, Bubasaur, Bellsprout)
-//  eevee evolutions
-//  pikachu evolution tree (Pikachu, )
-//  legendary pokemon
-//  Kanto region pokemon
-//  no evolution (Farfetch'd, )
-//  starting pokemon in kanto region
-//  pokedex no.1 pokemon (Bubasaur, Charmander, Squirtle , Pikachu.)
-//  pikachu type
+//  pikachu type (Dragon, Fairy, .Lightning, Normal)2
+//  pokedex no.1 pokemon (Squirtle, Mew, .Bubasaur, Pikachu)2
+//  Kanto region pokemon ( .Togepi, Dewgong, Porygon, Bellsprout)0
+//  Eevee evolutions (Suicune, Houndoom, Luxray, .Espeon)3
+//  pikachu evolution tree (.Pichu, Mimikyu, Emolga, Dedenne)0
+//  fairy type pokemon ( Snubbull, Mawile, .Cherubi, Morelull)2
+//  legendary pokemon (Rapdash, .Articuno, Aggron, Gardevoir)1
+//  dual type (Charizard, .Nidorina, Delibird, Oddish)2
+//  aloha forms (Diglett, Vulpix, Exeggutor, .Shellder)3
+//  no evolution (Wailmer, .Farfetch'd, Clefairy, Marill)1
 
 // show score correct out of /10
 
