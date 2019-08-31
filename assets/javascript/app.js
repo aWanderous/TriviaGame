ca977eac2af5da5279b6f673e2192c1a9714b63c
Alex Wan <a_wanderous@hotmail.com>
2019-08-31 16:16:00 +1000

    making images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,55 +4,55 @@ $(document).ready(function () {
     
     var questions = [
     {   //  pikachu type (Dragon, Fairy, .Lightning, Normal)
-        question: "What type of Pokemon is Pikachu?",
+        question: "What type of Pokémon is Pikachu?",
         choices: ["Dragon", "Fairy", "Lightning", "Normal"],
         answer: "Lightning"
     },
-    {   //  pokedex no.1 pokemon (Squirtle, Mew, .Bubasaur, Pikachu)
-        question: "Which Pokemon is number 1 in the pokedex?",
+    {   //  Pokédex no.1 Pokémon (Squirtle, Mew, .Bubasaur, Pikachu)
+        question: "Which Pokémon is number 1 in the Pokédex?",
         choices: ["Squirtle", "Mew", "Bulbasaur", "Pikachu"],
         answer: "Bulbasaur"
     }, 
     {   //  no evolution (Wailmer, .Farfetch'd, Clefairy, Marill)
-        question: "Which Pokemon does not have an evolution form?",
+        question: "Which Pokémon does not have an evolution form?",
         choices: ["Wailmer", "Farfetch'd", "Clefairy", "Marill"],
         answer: "Farfetch'd"
     },
-    {   //  Kanto region pokemon ( .Togepi, Dewgong, Porygon, Bellsprout)
-        question: "Which Pokemon is not from the Kanto region?",
+    {   //  Kanto region Pokémon ( .Togepi, Dewgong, Porygon, Bellsprout)
+        question: "Which Pokémon is not from the Kanto region?",
         choices: ["Togepi", "Dewgong", "Porygon", "Bellsprout"],
         answer: "Togepi"
     },
     {   //  pikachu evolution tree (.Pichu, Mimikyu, Emolga, Dedenne)
-        question: "Which Pokemon is of the Pikachu evolution tree?",
+        question: "Which Pokémon is of the Pikachu evolution tree?",
         choices: ["Pichu", "Mimikyu", "Emolga", "Dedenne"],
         answer: "Pichu"
     },
-    {   //  fairy type pokemon ( Snubbull, Mawile, .Cherubi, Morelull)
-        question: "Which Pokemon is not a fairy type?",
+    {   //  fairy type Pokémon ( Snubbull, Mawile, .Cherubi, Morelull)
+        question: "Which Pokémon is not a fairy type?",
         choices: ["Snubbull", "Mawile", "Cherubi", "Morelull"],
         answer: "Cherubi"
     },
     {   //  dual type (Charizard, .Nidorina, Delibird, Oddish)
-        question: "Which Pokemon does not have duel type?",
+        question: "Which Pokémon does not have duel type?",
         choices: ["Charizard", "Nidorina", "Delibird", "Oddish"],
         answer: "Nidorina"
     },    
-    {   //  legendary pokemon (Rapdash, .Articuno, Aggron, Gardevoir)1
-        question: "Which Pokemon is a legendary?",
+    {   //  legendary Pokémon (Rapdash, .Articuno, Aggron, Gardevoir)1
+        question: "Which Pokémon is a legendary?",
         choices: ["Rapdash", "Articuno", "Aggron", "Gardevoir"],
         answer: "Articuno"
     },
     {   //  Eevee evolutions (Suicune, Houndoom, Luxray, .Espeon)
-        question: "Which Pokemon is an Eevee evolution?",
+        question: "Which Pokémon is an Eevee evolution?",
         choices: ["Suicune", "Houndoom", "Luxray", "Espeon"],
         answer: "Espeon"
     },
     {   //  aloha forms (Diglett, Vulpix, Exeggutor, .Shellder)
-        question: "Which Pokemon does not have an Aloha form?",
+        question: "Which Pokémon does not have an Aloha form?",
         choices: ["Diglett", "Vulpix", "Exeggutor", "Shellder"],
         answer: "Shellder"
-    },]    
+    }]    
 console.log(questions)
 
     var timer = 30;
@@ -63,18 +63,27 @@ console.log(questions)
     // set start screen
     // button to start game
     $("#start").click(function(){
-        $("#start, header").hide();
+        $("#start, h1").hide();
+        $("#logo").attr("src","assets/images/Pokémon_logo.png");
         
         function quiz() {
-            
-            var ask  = $("#question").text(questions[currentQuestion].question)
-            var picks =  questions[currentQuestion].choices
-        }
+            $("#timer").text("Timer: " + timer);
+            var now = questions[currentQuestion];
+            var ask  = $("#question").text(now.question);
+            var picks = now.choices;
+            for (var i =0; i < picks.length; i++) {
+                var selections = $("<img>")
+                selections.attr("src", "assets/images/" + picks[i] + ".png");
+                selections.append($("#multi"));
+
+            }
+        };
+        quiz();
+    });
         
-        quiz()
-    })
     // timer starts when pressed
-    
+    var clock = setInterval(timer--, 1000);
+console.log(clock)
     
         
         
